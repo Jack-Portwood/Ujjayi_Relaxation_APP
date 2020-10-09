f4e2e07eb634c6343658afdcb546c5545f46cdf5
Jack Portwood <Jack.Portwood14@gmail.com>
2020-10-09 12:41:36 +0100

    file struction set out

---
+++ src/components/BreatheComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const BreatheComponent =()=> {
+
+    return (
+      <div>
+        <h1 className="Title">Ujjayi</h1>
+        
+      </div>
+    );
+
+
+}
+
+export default BreatheComponent
